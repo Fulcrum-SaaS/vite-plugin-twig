7558249f0a5754724e47f7bcd468c048fa2b56c2
C Brown <colyn.brown@fulcrumsaas.com>
2024-01-17 17:38:51 -0500

    Made updates to vite

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,26 +1,23 @@
-import path from 'node:path'
-import type { Plugin } from 'vite'
-import type { PluginOptions } from '../types'
-import { configureTwig, parseHTML, retrieveConfigFromFile } from './tasks'
+import path from "node:path";
+import { type IndexHtmlTransformContext, type Plugin } from "vite";
+import { configureTwig, parseHTML, retrieveConfigFromFile } from "./tasks";
 
-export async function viteTwigPlugin(options: PluginOptions): Promise<Plugin> {
-
-  const config = options || await retrieveConfigFromFile()
-
-  configureTwig(config)
-
+export async function viteTwigPlugin(): Promise<Plugin> {
+  const config = await retrieveConfigFromFile();
+  configureTwig(config);
   return {
-    name: 'vite-plugin-twig',
+    name: "vite-plugin-twig",
+
     transformIndexHtml: {
-      enforce: 'pre',
-      transform: async (html, ctx) => {
-        return await parseHTML(html, ctx, config)
-      }
+      order: "pre",
+      handler: async (html: string, ctx: IndexHtmlTransformContext) => {
+        return await parseHTML(html, ctx, config);
+      },
     },
     handleHotUpdate({ file, server }) {
-      if (path.extname(file) === '.twig') {
-        server.ws.send({ type: 'full-reload' })
+      if (path.extname(file) === ".twig") {
+        server.ws.send({ type: "full-reload" });
       }
-    }
-  }
+    },
+  };
 }
